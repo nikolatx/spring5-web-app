4ebdcd85ad974507f937bbffad30ac6dc78befa3
Nikola Timotijevic <nikolatx@gmail.com>
2021-01-07 14:12:18 +0100

    Adding a Publisher (@OneToOne to Book)

--- src/main/java/tnt/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/tnt/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -5,34 +5,46 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import tnt.springframework.spring5webapp.model.Author;
 import tnt.springframework.spring5webapp.model.Book;
+import tnt.springframework.spring5webapp.model.Publisher;
 import tnt.springframework.spring5webapp.repositories.AuthorRepository;
 import tnt.springframework.spring5webapp.repositories.BookRepository;
+import tnt.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
+        Publisher hc=new Publisher("Harper Collins", "HC address");
+        publisherRepository.save(hc);
         Author eric = new Author("Eric", "Evans");
-        Book book1 = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book book1 = new Book("Domain Driven Design", "1234", hc);
         eric.getBooks().add(book1);
         book1.getAuthors().add(eric);
+        hc.setBook(book1);
         authorRepository.save(eric);
         bookRepository.save(book1);
+        publisherRepository.save(hc);
 
+        Publisher wrox=new Publisher("Wrox", "Wrox address");
+        publisherRepository.save(wrox);
         Author rod=new Author("Rod", "Johnson");
-        Book noEJB=new Book("J2EE Development without EJB", "2344", "Wrox");
+        Book noEJB=new Book("J2EE Development without EJB", "2344", wrox);
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        wrox.setBook(noEJB);
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(wrox);
     }
 
     @Override
---
+++ src/main/java/tnt/springframework/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package tnt.springframework.spring5webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import tnt.springframework.spring5webapp.model.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
