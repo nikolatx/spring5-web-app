b0890785245fe33ca5ebbba8326fc4a46d34281a
Nikola Timotijevic <nikolatx@gmail.com>
2021-01-07 17:58:03 +0100

    Adding lists of books and authors

---
+++ src/main/java/tnt/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package tnt.springframework.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import tnt.springframework.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+    }
+
+}
